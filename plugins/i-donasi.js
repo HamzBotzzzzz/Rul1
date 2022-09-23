5d22f1c31fd56a658595307da5b2f95f698b5238
Hamzzzzz <109900268+HamzBotzzzzz@users.noreply.github.com>
2022-09-23 17:27:55 +0700

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,13 +8,13 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • telkom [0851251549899]
-│ • Gopay  [081352848936]
-│ • Dana  [081251549899]
-│ • Saweria  [https://saweria.co/arullofc]
+│ • telkom [088214989352]
+│ • Gopay  [088214989352]
+│ • Dana  [088214989352]
+│ • Saweria  [https://saweria.co/ZxynnBotz]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6285822347348
+│ > Ingin donasi? Wa.me/6288214989352
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
